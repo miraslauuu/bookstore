c42b0fd54d72ce2a18606f2776cf3c74949ba486
miraslauuu <248655@edu.p.lodz.pl>
2024-07-18 19:41:12 +0200

    *just fixing the extensions of the recently added files*

--- OnlineBookstore/obj/Debug/net8.0/OnlineBookstore.AssemblyInfo.cs
+++ OnlineBookstore/obj/Debug/net8.0/OnlineBookstore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookstore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcaf41e78163ed2bf2d9ac17968920bdc9fa3a97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d5ccae92f52c5498f2d67f3ffc6b2519b9d3ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookstore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookstore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
